4cb4431f13ecc7d1e0b2f78658a198d514268fbb
juancolchete <juanudkp@gmail.com>
2026-01-30 16:23:01 -0300

    fix: 6. URL Parameter Not Decoded

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,13 +1,4 @@
-function GetURLParameter(sParam)
-{
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++) 
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam) 
-        {
-            return sParameterName[1];
-        }
-    }
+function GetURLParameter(sParam) {
+    const params = new URLSearchParams(window.location.search)
+    return params.get(sParam)  // Automatically decoded
 }
