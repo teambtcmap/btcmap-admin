9c296f01f5abf66c2a28be1e78249803f55cceb1
juancolchete <juanudkp@gmail.com>
2026-02-01 10:22:41 -0300

    refactor: 12. Missing Documentation

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,4 +1,19 @@
+ /**
+  * Passing the name of URL parameter we get its value  
+  * @param {string} sParam - The search string also known as URL parameter 
+  * @returns {string} - The URL parameter content
+  * @throws {} 
+  */
 function getURLParameter(sParam) {
     const params = new URLSearchParams(window.location.search)
     return params.get(sParam)
+}
+ /**
+  * Passing a text without formatation, this text is normalized to get its main data  
+  * @param {string} text - Unformated text 
+  * @returns {string} - Formatted text 
+  * @throws {} 
+  */
+function cleanIssueText(text) {
+    return text?.replace(/\n/g, ' ').trim() || ''
 }
